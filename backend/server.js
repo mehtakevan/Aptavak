50dc2335bc640f9b68d70babdde268e874dcb219
Kevan <msjsci2021ema210@gmail.com>
2024-03-30 00:06:41 +0530

    deployment update

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://aptavak-frontend.vercel.app/",
+    origin: "https://aptavak.vercel.app/",
     // credentials: true,
   },
 });
